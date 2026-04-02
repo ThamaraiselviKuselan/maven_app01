45110a4b13dfe23e9316638c6ac994d6c86f5540
ThamaraiselviKuselan <thamaraisanthi1459@gmail.com>
2026-04-02 14:49:23 +0530

    Implement unit tests for App class methods
    Added unit tests for average calculation and grading.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,40 @@
+java
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    App student = new App();
+
+    @Test
+    public void testCalculateAverage() {
+        assertEquals(85.0, student.calculateAverage(85, 90, 80), 0.01);
+    }
+
+    @Test
+    public void testGradeA() {
+        assertEquals("A", student.calculateGrade(95));
+    }
+
+    @Test
+    public void testGradeB() {
+        assertEquals("B", student.calculateGrade(80));
+    }
+
+    @Test
+    public void testGradeC() {
+        assertEquals("C", student.calculateGrade(65));
+    }
+
+    @Test
+    public void testGradeD() {
+        assertEquals("D", student.calculateGrade(45));
+    }
+
+    @Test
+    public void testGradeF() {
+        assertEquals("F", student.calculateGrade(30));
+    }
+}
